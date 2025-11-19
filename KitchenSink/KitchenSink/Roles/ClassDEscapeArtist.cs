34a251d999d13fe3a01bcff95af83d3ac6223ad6
JimmyDogFsh <jimmydogfsh6@gmail.com>
2025-11-18 21:44:22 -0600

    CDEscapeArtist

--- KitchenSink/KitchenSink/Roles/ClassDEscapeArtist.cs
+++ KitchenSink/KitchenSink/Roles/ClassDEscapeArtist.cs
@@ -1,4 +1,4 @@
-using Exiled.API.Features.Attributes;
+﻿using Exiled.API.Features.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,64 +25,62 @@ using PlayerEvent = Exiled.Events.Handlers.Player;
 
 namespace KitchenSink.Roles
 {
-    [CustomRole(RoleTypeId.ClassD)]
+	[CustomRole(RoleTypeId.ClassD)]
 
-    public class ClassDEscapeArtist : CustomRole
-    {
+	public class ClassDEscapeArtist : CustomRole
+	{
 
-        public override RoleTypeId Role { get; set; } = RoleTypeId.ClassD;
+		public override RoleTypeId Role { get; set; } = RoleTypeId.ClassD;
 
-        public override uint Id { get; set; } = 69;
-        public override int MaxHealth { get; set; } = 110;
-        public override string Name { get; set; } = "Class-D Escape Artist";
-        public override string Description { get; set; } = "You are given a head-start over all other D Class.";
-        public override string CustomInfo { get; set; } = "Class-D Escape Artist";
-        public override List<string> Inventory { get; set; } = new List<string>()
-        {
-            $"{ItemType.KeycardO5}",
-            $"{ItemType.ArmorLight}",
-            $"{ItemType.GrenadeHE}",
-            $"{ItemType.SCP500}",
-            $"{ItemType.GunShotgun}",
-            $"{ItemType.Medkit}",
-        };
+		public override uint Id { get; set; } = 69;
+		public override int MaxHealth { get; set; } = 110;
+		public override string Name { get; set; } = "Class-D Escape Artist";
+		public override string Description { get; set; } = "You are given a head-start over all other D Class.";
+		public override string CustomInfo { get; set; } = "Class-D Escape Artist";
+		public override List<string> Inventory { get; set; } = new List<string>()
+		{
+			$"{ItemType.KeycardO5}",
+			$"{ItemType.ArmorLight}",
+			$"{ItemType.GrenadeHE}",
+			$"{ItemType.SCP500}",
+			$"{ItemType.GunShotgun}",
+			$"{ItemType.Medkit}",
+		};
 
-        public override Dictionary<AmmoType, ushort> Ammo { get; set; } = new Dictionary<AmmoType, ushort>()
-        {
-            { AmmoType.Ammo12Gauge, 22 }
-        };
+		public override Dictionary<AmmoType, ushort> Ammo { get; set; } = new Dictionary<AmmoType, ushort>()
+		{
+			{ AmmoType.Ammo12Gauge, 22 }
+		};
 
-        public override SpawnProperties SpawnProperties { get; set; } = new SpawnProperties()
-        {
-            Limit = 1,
-            RoomSpawnPoints = new List<RoomSpawnPoint>
-            {
-             new RoomSpawnPoint()
-             {
+		public override SpawnProperties SpawnProperties { get; set; } = new SpawnProperties()
+		{
+			Limit = 1,
+			RoomSpawnPoints = new List<RoomSpawnPoint>
+			{
+			 new RoomSpawnPoint()
+			 {
 
-                Room = RoomType.HczIncineratorWayside,
-                Chance = 10,
-                Offset = new Vector3(0,2,0),
-             },
-
-
+				Room = RoomType.HczIncineratorWayside,
+				Chance = 10,
+				Offset = new Vector3(0,2,0),
+			 },
 
 
 
 
 
-              },
-        };
-        public override List<CustomAbility> CustomAbilities { get; set; } = new List<CustomAbility>()
-        {
-            new TestSubject()
-            {
-                Name = "Test Subject",
-                Description = "Gives effects of SCP-207 For the durration of the round",
-            },
-        };
 
 
+			  },
+		};
+		public override List<CustomAbility> CustomAbilities { get; set; } = new List<CustomAbility>()
+		{
+			new TestSubject()
+			{
+				Name = "Test Subject",
+				Description = "Gives effects of SCP-207 For the durration of the round",
+			},
+		};
 
 
 
@@ -95,5 +93,7 @@ namespace KitchenSink.Roles
 
 
 
-    }
+
+
+	}
 }
