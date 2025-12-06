1b7042950fa714499e0bd7af4a84f3d8a469bda0
CosmicCanister <cosmiccanister@gmail.com>
2025-12-06 01:29:22 -0600

    Dodging a bullet with accidentally deleted files

---
+++ KitchenSink/KitchenSink/Abilities/Zombieinfect.cs
@@ -0,0 +1,43 @@
+﻿﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Exiled.API.Features;
+using Exiled.CustomRoles.API.Features;
+using Exiled.Events.EventArgs.Player;
+using MEC;
+using PlayerRoles;
+
+namespace KitchenSink.Abilities
+{
+    public class Zombieinfect : PassiveAbility
+    {
+        public override string Name { get; set; } = "ZombieInfect";
+        public override string Description { get; set; } = "Infects people";
+
+        public RoleTypeId ConvertToRole { get; set; } = RoleTypeId.Scp0492;
+
+        protected override void AbilityAdded(Player player)
+        {
+            Exiled.Events.Handlers.Player.Dying += OnDying;
+        }
+
+        protected override void AbilityRemoved(Player player)
+        {
+            Exiled.Events.Handlers.Player.Dying -= OnDying;
+        }
+
+        private void OnDying(DyingEventArgs ev)
+        {
+            if (ev.Attacker == null || ev.Player == null)
+                return;
+            if (Check(ev.Attacker))
+            {
+                Timing.CallDelayed(0.5f, () =>
+                {
+                    CustomRole zombie = Roles.SCP0081.Get(65);
+                    zombie.AddRole(ev.Player);
+
+                });
+            }
+        }
+    }
+}
